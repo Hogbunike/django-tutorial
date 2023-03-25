2b50f9b7f28f1bcbe5b5d5d5c7e2a3ab3ab47728
Henry <hogbunike@gmail.com>
2023-03-25 14:18:05 +0100

    Using database with django

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Venue
+from .models import Events
+from .models import MyClubUsers
 
-# Register your models here.
+admin.site.register(Venue)
+admin.site.register(Events)
+admin.site.register(MyClubUsers)
