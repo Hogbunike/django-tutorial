986ef435b51059ffb76765cdc90d8b1fa52b6154
Henry <hogbunike@gmail.com>
2023-04-03 17:48:08 +0100

    Create an Add event page

--- events/forms.py
+++ events/forms.py
@@ -1,6 +1,24 @@
 from django import forms
 from django.forms import ModelForm
-from .models import Venue
+from .models import Venue, Events
+
+class EventForm(ModelForm):
+    class Meta:
+        model = Events
+        fields = ('name', 'event_date', 'venue', 'manager', 'description', 'attendees')
+        labels = {
+            'name': "",
+            'address': "",
+            'phone': "",
+        }
+
+        widgets = {
+            'name': forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Event Name'}),
+            'event_date': forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Event date'}),
+            'venue': forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Venue'}),
+            'manager': forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Manager'}),
+            'description': forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Description'})
+        }
 
 # create a venue form
 class VenueForm(ModelForm):
--- events/urls.py
+++ events/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
     path('show_venue/<venue_id>', views.show_venue, name='show-venue'),
     path('search_venues', views.search_venues, name='search_venues'),
     path('update_venue/<venue_id>', views.update_venue, name='update_venue'),
+    path('add_event', views.add_event, name='add-event'),
+
 
 
 ]
