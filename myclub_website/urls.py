41b00cc0e400eb70cff0fc645b9745e5980aa6b1
Henry <hogbunike@gmail.com>
2023-04-12 16:57:17 +0100

    Costomize Admin titles and Login with user Authentication

--- myclub_website/urls.py
+++ myclub_website/urls.py
@@ -4,4 +4,11 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('events.urls')),
+    path('members/',include('django.contrib.auth.urls')),
+    path('members/', include('members.urls')),
+
 ]
+# configure Admin Titles
+admin.site.site_header = "My Club Administration Page"
+admin.site.site_title = "Browser title"
+admin.site.index_title = "Welcome to the Admin Area..."
