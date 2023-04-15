18115766cd7c3d73c838a47a623b9b73e7987993
Henry <hogbunike@gmail.com>
2023-04-15 18:44:30 +0100

    Register Users

--- members/views.py
+++ members/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 def login_user(request):
     if request.method == "POST":
@@ -22,3 +23,5 @@ def logout_user(request):
     messages.success(request, ("you were logged out"))
     return redirect('home')
 
+def register_user(request):
+    return render(request, 'authenticate/register_user.html', {})
